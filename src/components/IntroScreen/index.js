ada860c0191cdae8d62c1bfcb325395385a1bd67
Gustavo Cardoso <gustavocardoso@gmail.com>
2020-11-12 11:12:39 -0800

    Change Logo component path

--- src/components/IntroScreen/index.js
+++ src/components/IntroScreen/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useCallback } from "react";
 import PropTypes from "prop-types";
 
-import Logo from "../Shared/Logo";
+import Logo from "../../components/Shared/Logo";
 import { StartButton } from "./styles";
 
 const IntroScreen = ({ startApp }) => {
