c36c3080b865a1ea413d5f2203130836e0f1a788
Gustavo Cardoso <gustavocardoso@gmail.com>
2020-11-12 11:27:21 -0800

    Change Logo component path

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 import Stage from "./Stage";
 import Keyboard from "./Keyboard";
 import Player from "./Player";
-import Logo from "../Common/L ogo";
+import Logo from "../Common/Logo";
 
 import alphabetData from "../../data/letters.json";
 
